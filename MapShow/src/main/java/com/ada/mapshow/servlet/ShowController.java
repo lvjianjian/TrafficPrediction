45ea2491d6cbc97c082926bfce85623cd7e48c34
Zhongjian Lv <568066242@qq.com>
2017-07-17 15:10:38 +0800

    finish LinearInterpolationFixed

--- MapShow/src/main/java/com/ada/mapshow/servlet/ShowController.java
+++ MapShow/src/main/java/com/ada/mapshow/servlet/ShowController.java
@@ -29,8 +29,11 @@ public class ShowController {
 
     private double[] region = new double[]{116.26954, 39.828598, 116.49167, 39.997132};
     //    private String speedPath2 = Parameter.PROJECTPATH + "data" + File.separator + "5TimeWindow_10MinEdges_maxMaxEdges_0MinSectionLength" + File.separator + "48_48_20";
-    private String speedPath = Parameter.PROJECTPATH + "data" + File.separator + "avgspeedfromrow" + File.separator + "2016" + File.separator + "03" + File.separator + "48_48_20";
+    private String rawSpeedPath = Parameter.PROJECTPATH + "data" + File.separator + "avgspeedfromrow" + File.separator + "2016" + File.separator + "03" + File.separator + "48_48_20";
     private String maxFixedSpeedPath = Parameter.PROJECTPATH + "data" + File.separator + "avgspeedfromrow" + File.separator + "2016" + File.separator + "03" + File.separator + "48_48_20_MaxSpeedFillingFixed_20";
+    private String inearInterpolationSpeedFixed = Parameter.PROJECTPATH + "data" + File.separator + "avgspeedfromrow" + File.separator + "2016" + File.separator + "03" + File.separator + "48_48_20_LinearInterpolationFixed";
+    private String speedPath = inearInterpolationSpeedFixed;
+
 
     @RequestMapping(value = "/map.do")
     public String show() {
@@ -47,15 +50,14 @@ public class ShowController {
         List<Long>[] lists = Tool.roadClassificationByNum(RoadsWithNum.load(Parameter.PROJECTPATH + "result\\r_min100"), split, null);
         List<double[]>[] doubles = new ArrayList[lists.length];
         Map<Long, Vertex> vertexMap = load.getVertexMap();
-        for (int i = 0; i < lists.length; i++) {
+        for (int i = 0; i < lists.length; i++)
+        {
             List<Long> list = lists[i];
             doubles[i] = new ArrayList<>();
-            for (int j = 0; j < list.size(); j++) {
+            for (int j = 0; j < list.size(); j++)
+            {
                 Edge edge = load.getEdgeMap().get(list.get(j));
-                doubles[i].add(new double[]{vertexMap.get(edge.getStartVertex()).getLongitude(),
-                        vertexMap.get(edge.getStartVertex()).getLatitude(),
-                        vertexMap.get(edge.getEndVertex()).getLongitude(),
-                        vertexMap.get(edge.getEndVertex()).getLatitude()});
+                doubles[i].add(new double[]{vertexMap.get(edge.getStartVertex()).getLongitude(), vertexMap.get(edge.getStartVertex()).getLatitude(), vertexMap.get(edge.getEndVertex()).getLongitude(), vertexMap.get(edge.getEndVertex()).getLatitude()});
             }
         }
         HashMap<String, Object> result = new HashMap<>();
@@ -67,7 +69,7 @@ public class ShowController {
     @RequestMapping(value = "/times.json", method = RequestMethod.POST)
     @ResponseBody
     public Map<String, Object> getTimes() {
-        AllAvgSpeedCondition load = AllAvgSpeedCondition.load(maxFixedSpeedPath);
+        AllAvgSpeedCondition load = AllAvgSpeedCondition.load(speedPath);
         HashMap<String, Object> objectObjectHashMap = new HashMap<>();
         objectObjectHashMap.put("xnum", load.getX_num());
         objectObjectHashMap.put("ynum", load.getY_num());
@@ -79,7 +81,7 @@ public class ShowController {
     @RequestMapping(value = "/getByTime.json", method = RequestMethod.POST)
     @ResponseBody
     public HashMap<String, Object> getAvgSpeedConditionByTime(String time) {
-        AllAvgSpeedCondition load = AllAvgSpeedCondition.load(maxFixedSpeedPath);
+        AllAvgSpeedCondition load = AllAvgSpeedCondition.load(speedPath);
         AvgSpeedCondition avgSpeedCondition = load.get(time);
         float[][] speeds = avgSpeedCondition.getSpeeds();
         int[][] weights = avgSpeedCondition.getWeight();
@@ -94,18 +96,23 @@ public class ShowController {
     @ResponseBody
     public List<Trajectory> getSomeTrajs() {
         List<Trajectory> trajectories = new ArrayList<>();
-        try {
+        try
+        {
             BufferedReader bufferedReader = new BufferedReader(new FileReader(Parameter.PROJECTPATH + "data\\20160229233000_20160301000000"));
             String s = null;
-            while ((s = bufferedReader.readLine()) != null) {
-                if (s.contains(",")) {
+            while ((s = bufferedReader.readLine()) != null)
+            {
+                if (s.contains(","))
+                {
                     Trajectory trajectory = Trajectory.readOne(s);
                     trajectories.add(trajectory);
                 }
             }
-        } catch (FileNotFoundException e) {
+        } catch (FileNotFoundException e)
+        {
             e.printStackTrace();
-        } catch (IOException e) {
+        } catch (IOException e)
+        {
             e.printStackTrace();
         }
         return trajectories;
