d0d03ded9716f0585ff4b385bb7c095eee5166ad
Zhongjian Lv <568066242@qq.com>
2017-09-18 09:58:16 +0800

    add visulization

---
+++ JamPredict/visulize/fig_plot.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+# encoding=utf-8
+
+"""
+@Author: zhongjianlv
+
+@Create Date: 17-9-18, 09:56
+
+@Description:
+
+@Update Date: 17-9-18, 09:56
+"""
+
+import matplotlib
+
+matplotlib.use("Agg")
+
+import matplotlib.pyplot as plt
+
+if __name__ == '__main__':
+    pass
