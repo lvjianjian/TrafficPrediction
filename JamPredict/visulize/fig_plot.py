9aa3947d43832e3f04db802ecbce54e0263111a4
Zhongjian Lv <568066242@qq.com>
2017-09-18 11:52:48 +0800

    plot

--- JamPredict/visulize/fig_plot.py
+++ JamPredict/visulize/fig_plot.py
@@ -14,8 +14,22 @@
 import matplotlib
 
 matplotlib.use("Agg")
-
 import matplotlib.pyplot as plt
+import os
+import time
+from jampredict.utils.Cache import *
+from jampredict.utils import Paramater
+from jampredict.feature import Data
+import seaborn as ses
 
 if __name__ == '__main__':
-    pass
+    datas, times, x_num, y_num, interval, startTime, endTime, nospeed_regions = \
+        Data.loadRawData(os.path.join(Paramater.DATAPATH, "2016/all/48_48_20_LinearInterpolationFixed"),
+                         os.path.join(Paramater.DATAPATH, "48_48_20_noSpeedRegion_0.05"), False)
+    x_index = 24
+    y_index = 24
+    datas = datas[:, 0]
+    f, ax = plt.subplots(1, 1, figsize=(15, 7))
+    print datas[:500, x_index, y_index]
+    ses.tsplot(datas[:500, x_index, y_index], ax=ax)
+    plt.savefig(os.path.join(Paramater.PROJECTPATH, "fig/test2.jpg"))
